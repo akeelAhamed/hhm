b0b0bf4849622192d104e8f8493151b18d5019d2
Akeel ahamed <ahamedalakeel@gmail.com>
2020-07-24 18:49:33 +0530

    base component used to extend insted of react

---
+++ src/components/BaseComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/**
+ * Base component for this application & to be extented to all component
+ */
+export default class BaseComponent extends React.Component {
+  
+  constructor(props){
+    super();
+  }
+
+
+}
