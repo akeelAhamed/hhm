21258b86eabcf3fb55ada7d4871a0113f8f246d8
Akeel ahamed <ahamedalakeel@gmail.com>
2020-07-26 23:42:13 +0530

    Api error cleared

--- src/components/BaseComponent.js
+++ src/components/BaseComponent.js
@@ -10,34 +10,17 @@ export default class BaseComponent extends React.Component {
   constructor(props) {
     super();
     this.api = Axios;
-    this.api.defaults.baseURL = 'http://www.hhmlife.org/';
-    //this.api.defaults.baseURL = 'https://reqres.in/api/';
+    this.api.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/http://www.hhmlife.org/';
     this.api.defaults.headers.common['APP_KEY'] = 'Test@123';
-    //this.api.defaults.headers.common['Accept']  = 'application/json';
     this.api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
     this.api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
     this.api.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
-    //this.api.defaults.withCredentials = true;
-    //this.api.defaults.crossDomain = true;
   }
 
   /**
    * Init base function
    */
   init(){
-    // fetch("http://www.hhmlife.org/")
-    //   .then(res => res.json())
-    //   .then(
-    //     (result) => {
-    //       console.log(result);
-    //     },
-    //     // Note: it's important to handle errors here
-    //     // instead of a catch() block so that we don't swallow
-    //     // exceptions from actual bugs in components.
-    //     (error) => {
-          
-    //     }
-    //   )
     this.api.get('/')
       .then((result) => {
         console.log(result);
